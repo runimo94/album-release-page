a7657c485924a9831b9ee8ff20d27c4a9924503f
runimo94 <runimo94git@gmail.com>
2023-01-31 16:39:16 +0100

    data stream it component

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Artist } from '../interfaces/artist.interface';
+import { Stream } from '../interfaces/stream.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -18,6 +19,35 @@ export class DataService {
     {
       name: 'Goiko',
       imgUrl: '../assets/images/artist2.webp',
+    },
+  ];
+
+  //Datos stream it component
+  public listStreams: Stream[] = [
+    {
+      name: 'Bandcamp',
+      urlImg: '../assets/social/monstercatplayer.svg',
+      url: '',
+    },
+    {
+      name: 'Soundcloud',
+      urlImg: '../assets/social/monstercatplayer.svg',
+      url: '',
+    },
+    {
+      name: 'Apple music',
+      urlImg: '../assets/social/monstercatplayer.svg',
+      url: '',
+    },
+    {
+      name: 'Youtube',
+      urlImg: '../assets/social/monstercatplayer.svg',
+      url: '',
+    },
+    {
+      name: 'Spotify',
+      urlImg: '../assets/social/monstercatplayer.svg',
+      url: '',
     },
   ];
 }
