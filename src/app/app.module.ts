f15e586837377c82562f7f8cd95fe47d9844e042
runimo94 <runimo94git@gmail.com>
2023-02-01 21:23:08 +0100

    footer component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { MainComponent } from './components/main/main.component';
 import { StreamItComponent } from './components/stream-it/stream-it.component';
 import { TrackListComponent } from './components/track-list/track-list.component';
 import { OtherReleasesComponent } from './components/other-releases/other-releases.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, SocialComponent, HeaderComponent, MainComponent, StreamItComponent, TrackListComponent, OtherReleasesComponent],
+  declarations: [AppComponent, SocialComponent, HeaderComponent, MainComponent, StreamItComponent, TrackListComponent, OtherReleasesComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
