1b62352046c40c3bb0e6d520559d3c6ceae3a16b
runimo94 <runimo94git@gmail.com>
2023-02-03 15:44:19 +0100

    desplegables sidenav

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,17 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  faAngleUp = faAngleUp;
+
   private sidenav?: HTMLElement | null;
+
+  constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
     this.sidenav = document.getElementById('rightSideNav');
   }
 
-  open() {
-    this.sidenav?.classList.remove('hidden');
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (!this.el.nativeElement.contains(event.target)) {
+      this.close();
+    }
   }
 
   close() {
     this.sidenav?.classList.add('hidden');
+  }
+
+  handleSideNav() {
+    this.sidenav?.classList.toggle('hidden');
+  }
+
+  show(event: any) {
+    const target = event.target as HTMLElement;
+    const parent = target.closest('li');
+    const ulElement = parent?.querySelector('ul.childList');
+    const faIconElement = target.querySelector('fa-icon');
+
+    ulElement?.classList.toggle('hidden');
+    ulElement?.classList.toggle('flex');
+
+    faIconElement?.classList.toggle('fa-rotate-90');
   }
 }
